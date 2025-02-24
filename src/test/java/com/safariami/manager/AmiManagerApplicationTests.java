144810f3ede02c2322d8832841324c5becb2dddc
shutiye1 <shutiye@beco.so>
2025-02-24 09:59:06 +0300

    fixing build error

--- src/test/java/com/safariami/manager/AmiManagerApplicationTests.java
+++ src/test/java/com/safariami/manager/AmiManagerApplicationTests.java
@@ -2,8 +2,9 @@ package com.safariami.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import safariami.manager.Application;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class AmiManagerApplicationTests {
 
 	@Test
